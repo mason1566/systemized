d0b9f0dfc80bad96cf7e3e7c470c2b461977476e
mason1566 <22926064+mason1566@users.noreply.github.com>
2025-08-17 20:07:51 -0700

    implement printing of capture devices along with playback devices

--- src/AudioIO.cpp
+++ src/AudioIO.cpp
@@ -45,16 +45,24 @@ void AudioIO::printDevices()
     ma_uint32 playbackCount;
     ma_device_info* pCaptureInfos;
     ma_uint32 captureCount;
-    if (ma_context_get_devices(&getContext(), &pPlaybackInfos, &playbackCount, &pCaptureInfos, &captureCount) != MA_SUCCESS) {
+    if (ma_context_get_devices(&getContext(), &pPlaybackInfos, &playbackCount, &pCaptureInfos, &captureCount) != MA_SUCCESS) 
+    {
         // Error.
         throw std::runtime_error("Miniaudio Error: Context failed when fetching devices\n");
     }
 
-    std::cout << "\n";
+    std::cout << "\nPlayback Devices:\n";
 
     // Loop over each device info and do something with it. Here we just print the name with their index. You may want
     // to give the user the opportunity to choose which device they'd prefer.
-    for (ma_uint32 iDevice = 0; iDevice < playbackCount; iDevice++) {
-        std::cout << "[" << iDevice+1 << "] " << pPlaybackInfos[iDevice].name << "\n";
+    for (ma_uint32 i = 0; i < playbackCount; i++) 
+    {
+        std::cout << "  [" << i+1 << "] " << pPlaybackInfos[i].name << "\n";
+    }
+
+    std::cout << "\nCapture Devices:\n";
+    for (ma_uint32 i = 0; i < captureCount; i++)
+    {
+        std::cout << "  [" << i+1 << "] " << pCaptureInfos[i].name << "\n";
     }
 }
